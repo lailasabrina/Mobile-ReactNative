e013a50c719f81a9a0a1c61de45d7a7c8b46e60f
Laila Sabrina <ladysam.asl@hotmail.com>
2021-09-24 03:39:22 -0300

    Atividade 13

---
+++ 13-appMeuPerfil3/src/pages/Experiencia/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from '../../../style';
+ 
+export default function Experiencia() {
+ return (
+  <View style={styles.container2}>
+  <View style={styles.box2}>
+    <Text style={styles.titulo}>Experiência</Text>
+    <Text style={styles.textotitulo}>• Recep. Caixa</Text>
+    <Text style={styles.texto2}>Carrefour - São Vicente</Text>
+    <Text style={styles.textotitulo}>• Estágiaria</Text>
+    <Text style={styles.texto2}>Liceu Santista - Santos</Text>
+    <Text style={styles.textotitulo}>• Monitora</Text>
+    <Text style={styles.texto3}>Fatec - Santos/Rubens Lara</Text>
+    
+  </View>
+
+</View>
+  );
+}
