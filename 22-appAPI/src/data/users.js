31dd748995d940b949beff845e3d7a172c08e754
Laila Sabrina <ladysam.asl@hotmail.com>
2021-11-20 23:45:24 -0300

    Atividade 22

---
+++ 22-appAPI/src/data/users.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    name: 'Kim Seokjin',
+    email: 'worldwidehandsome@email.com',
+    avatarUrl: 'https://i.pinimg.com/originals/0c/0a/01/0c0a01cc935c85fe2ae0b18b5e30a01e.jpg'
+  },
+  {
+    id: 2,
+    name: 'Min Yoongi',
+    email: 'suga@email.com',
+    avatarUrl: 'https://64.media.tumblr.com/2e842177c2b6cad4adc77c80a50ea32f/e6489ec55857f9b8-42/s1280x1920/c01f98d22a936cc9de572e74aa0df678b61dfa46.jpg'
+  },
+  {
+    id: 3,
+    name: 'Jung Hoseok',
+    email: 'j-hope@email.com',
+    avatarUrl: 'https://i.pinimg.com/originals/8c/ba/f0/8cbaf06f161ac6d0b69be27cf4ea46d9.jpg'
+  },
+  {
+    id: 4,
+    name: 'Kim Namjoon',
+    email: 'rapmonster@email.com',
+    avatarUrl: 'https://i.pinimg.com/originals/c2/dd/85/c2dd85321baeb55022b9c4f3ab1e7c45.jpg'
+  },
+  {
+    id: 5,
+    name: 'Park Jimin',
+    email: 'chimmy@email.com',
+    avatarUrl: 'https://data.whicdn.com/images/341472134/original.jpg'
+  },
+  {
+    id: 6,
+    name: 'Kim Taehyung',
+    email: 'goodboy_taetae@email.com',
+    avatarUrl: 'https://64.media.tumblr.com/405ba117cf2ccb1699778dceb091af87/ec1f4a108892b39f-9d/s500x750/2e60bcf3665232320e63c5dc84bb2ca9f6761cbc.png'
+  },
+  {
+    id: 7,
+    name: 'Jeon Jungkook',
+    email: 'kookie@email.com',
+    avatarUrl: 'https://64.media.tumblr.com/a54d28f93fae2b21f1169d54c6e2881f/310ccb83a5c0f7e6-0e/s1280x1920/6690349d71a434a3a0fac0932808414127d11b31.png'
+  },
+]
